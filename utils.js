981f0596a64cb0bf3ca07816db6e0fffe04caf4a
CommanderRedYT <ccommanderred@gmail.com>
2023-04-14 18:05:20 +0200

    Updated stuff

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 export function fileSizeToString(size) {
-    const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const units = ['b', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'];
     const i = Math.floor(Math.log(size) / Math.log(1024));
     return `${(size / Math.pow(1024, i)).toFixed(2) * 1} ${units[i]}`;
 }
